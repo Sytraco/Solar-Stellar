7993badce0c739ac58d3004c97260821f91e434e
Sytraco <pointeau.simon@gmail.com>
2022-04-21 21:02:53 +0200

    numerical method

--- rungekutta.py
+++ rungekutta.py
@@ -63,23 +63,7 @@ class RungeKutta(MethodNum):
             self.A = np.roll(np.diag([0, 1/2, 1/2, 1]), -1)
             self.B = np.array([1/6, 1/3, 1/3, 1/6])
             self.C = np.array([0, 1/2, 1/2, 1])
-
-    # RK4 scheme solution from TP corrections
-    def rk2D(self, EDO, y0, nb_point=None):
-
-        y, y0, loop, _ = self.init_compute(y0=y0, stop_point=nb_point)
-
-        x = np.copy(self.x)
-        
-        for i in range(loop):
-            K1 = EDO(x_axis=x[i]     ,  ydy=y[:,i])
-            K2 = EDO(t=x[i]+self.Ts/2,  ydy=y[:,i]+self.Ts/2*K1)
-            K3 = EDO(t=x[i]+self.Ts/2,  ydy=y[:,i]+self.Ts/2*K2)
-            K4 = EDO(t=x[i]+self.Ts  ,  ydy=y[:,i]+self.Ts*K3)
-            y[:,i+1] = y[:,i] + self.Ts/6*(K1 + 2*K2 + 2*K3 + K4)
-            x[i+1] = x[i] + self.Ts
-
-        return y
+            
             
     # General scheme for RK order q
     def compute(self, EDO, y0, excitation=False, nb_point=None):
